a44f819a4f421105c45f19b686335afa9ce04a97
mpdev2311 <marcospdev2311gmail.com>
2022-01-03 14:53:18 -0300

    primeira aula before.js

---
+++ js-functions/foundation/before.js
@@ -0,0 +1,17 @@
+// 1. prerequisites
+
+// ->basic Knowledge of js
+// ---> Data types: number, string, boolean, array and object
+// ---> operators (+, -, *, /, &&, ||, >=, <=,...)
+// ---> If, For, while
+
+// 2. Node 
+// -> NodeJS (v14.6.0)
+
+// 3. Visual Studio code 
+// -> Code Runner
+//--> Code-runer: Clear Previous Output
+// --> Ctrl + Alt + n (run code)
+// --> Ctrl + Alt + n (atop running code)
+
+console.log('test! version 2!')
