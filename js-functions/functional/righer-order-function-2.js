6bf0258da4f4557a99a1faf33d821c5e91a7add6
mpdev2311 <marcospdev2311gmail.com>
2022-01-22 09:11:25 -0300

    righer-order-function-2.js

---
+++ js-functions/functional/righer-order-function-2.js
@@ -0,0 +1,9 @@
+function finalPrice(tax) {
+    return function(price) {
+        return price * (1 + tax)
+    }    
+}
+
+console.log(finalPrice(0.0875)( 25.1))
+console.log(finalPrice(0.0875)(21.7))
+console.log(finalPrice(0.0875)(107.9))
